c135a19130f8d6c860a574eac744c80b189bb716
Jonathan Diller <jonathan.a.diller@gmail.com>
2024-04-10 22:54:10 -0600

    chatter

--- basic_tutorial/basic_tutorial/publisher_member_function.py
+++ basic_tutorial/basic_tutorial/publisher_member_function.py
@@ -21,7 +21,7 @@ class MinimalPublisher(Node):
 
     def __init__(self):
         super().__init__('minimal_publisher')
-        self.publisher_ = self.create_publisher(String, 'topic', 10)
+        self.publisher_ = self.create_publisher(String, 'chatter', 10)
         timer_period = 0.5  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
--- basic_tutorial/basic_tutorial/subscriber_member_function.py
+++ basic_tutorial/basic_tutorial/subscriber_member_function.py
@@ -21,7 +21,7 @@ class MinimalSubscriber(Node):
 
     def __init__(self):
         super().__init__('minimal_subscriber')
-        self.subscription = self.create_subscription(String, 'topic', self.listener_callback, 10)
+        self.subscription = self.create_subscription(String, 'chatter', self.listener_callback, 10)
         self.subscription  # prevent unused variable warning
 
     def listener_callback(self, msg):
